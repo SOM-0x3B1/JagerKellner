791727891389c5a1d7509c56a9e811e957e2d174
SOM-0x3B1 <81362073+SOM-0x3B1@users.noreply.github.com>
2024-04-18 13:15:52 +0200

    Add USB UART

--- main.c
+++ main.c
@@ -1,14 +1,28 @@
 #include "project.h"
 
+
+void Init(void)
+{
+    VDAC8_1_Start();
+    I2C_GY87_Start();
+    USBUART_Start(0, USBUART_5V_OPERATION);
+    USBUART_CDC_Init();
+    while(USBUART_GetConfiguration() == 0) {}
+}
+
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
+    
+    Init();
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-
+    USBUART_PutString("COM Port Open");
+    
     for(;;)
     {
         /* Place your application code here. */
+        USBUART_PutString("ALMA\n");
     }
 }
 

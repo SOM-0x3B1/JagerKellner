8dcc92d03dbcd3c82a1cbeed340e367fdf4de1f9
SOM-0x3B1 <81362073+SOM-0x3B1@users.noreply.github.com>
2024-04-18 14:53:28 +0200

    Add motor control

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include "project.h"
 
 
-void Init(void)
+#define PWM_MAX 11999
+
+typedef enum {
+    MOTOR_GO,
+    MOTOR_BREAKING,
+    MOTOR_SLEEPING
+} MotorState;
+
+
+volatile float targetAngle = 0;
+volatile float currZAngle = 0;
+
+
+void init(void)
 {
     I2C_GY87_Start();
     USBUART_Start(0, USBUART_5V_OPERATION);
@@ -9,19 +22,36 @@ void Init(void)
     while(USBUART_GetConfiguration() == 0) {}
 }
 
+void setPWM(float mL, float mR){
+    int lPWM = (int)PWM_MAX * mL;
+    int rPWM = 1 - ((int)PWM_MAX * mR);   
+    PWM_Motor_WriteCompare1(lPWM);
+    PWM_Motor_WriteCompare1(rPWM);
+}
+
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
     
-    Init();
+    init();
+    
+    MotorState motorSate = MOTOR_GO;
+    float motorRPercentage = 0;
+    float motorLPercentage = 0;
+    
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-    USBUART_PutString("COM Port Open");
+    USBUART_PutString("COM Port Open\n");
     
     for(;;)
     {
-        /* Place your application code here. */
-        USBUART_PutString("ALMA\n");
+        switch(motorSate){
+            case MOTOR_GO:
+                setPWM(motorLPercentage, motorRPercentage);
+                break;
+            default:
+                break;
+        }
     }
 }
 

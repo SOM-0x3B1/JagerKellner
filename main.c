cc9432241919e780295872e6ba99926156391bcd
SOM-0x3B1 <somadagaszkar@gmail.com>
2024-04-20 17:20:54 +0200

    Add gyroscope data averaging

--- main.c
+++ main.c
@@ -1,8 +1,6 @@
 #include "project.h"
 #include "mpu6050.h"
 #include "stdio.h"
-#include "stdlib.h"
-#include "math.h"
 
 
 #define PWM_MAX 11999
@@ -13,35 +11,47 @@ typedef enum {
     MOTOR_SLEEPING
 } MotorState;
 
+typedef enum {
+    GYRO_SAMPLE,
+    GYRO_EVAL
+} GyroState;
+
 
 char usbOutBuf[100];
 
+
 volatile float targetAngle = 0;
 
+volatile GyroState gyroState = GYRO_SAMPLE;
 int16_t CAX, CAY, CAZ; //current acceleration values
 int16_t CGX, CGY, CGZ; //current gyroscope values
-int16_t CT;            //current temperature   
-float AXoff, AYoff, AZoff; //accelerometer offset values
-float GXoff, GYoff, GZoff; //gyroscope offset values
-int16_t AX, AY, AZ; //acceleration floats
-int16_t GX, GY, GZ; //gyroscope floats
+volatile float AXoff = 0, AYoff = 0, AZoff = 0; //accelerometer offset values
+volatile float GXoff = 0, GYoff = 0, GZoff = 0; //gyroscope offset values
+volatile int AX = 0, AY = 0, AZ = 0; //acceleration floats
+volatile int GX = 0, GY = 0, GZ = 0; //gyroscope floats
+volatile int sampleCount = 0;
 
 
 void calibrate(int numberOfTests){
- for(int i=0; i<numberOfTests; i++)
-    {   
-      MPU6050_getMotion6t(&CAX, &CAY, &CAZ, &CGX, &CGY, &CGZ, &CT);
-      AXoff += CAX;
-      AYoff += CAY;
-      AZoff += CAZ;
-      GXoff += CGX;
-      GYoff += CGY;
-      GZoff += CGZ;
+    for(int i=0; i<numberOfTests; i++) {   
+        MPU6050_getMotion6(&CAX, &CAY, &CAZ, &CGX, &CGY, &CGZ);
+        AXoff += CAX;
+        AYoff += CAY;
+        AZoff += CAZ;
+        GXoff += CGX;
+        GYoff += CGY;
+        GZoff += CGZ;
     
-      CyDelay(25);
-      sprintf(usbOutBuf, "\r%d / %d ", i + 1, numberOfTests);
-      USBUART_PutString(usbOutBuf);
+        CyDelay(25);
+        if((i & 3) == 0) { // Write only when i % 4 == 0
+            sprintf(usbOutBuf, "\r%d / %d ", i + 1, numberOfTests);
+            USBUART_PutString(usbOutBuf);
+        }
     }
+    
+    sprintf(usbOutBuf, "\r%d / %d \n\r", numberOfTests, numberOfTests);
+    USBUART_PutString(usbOutBuf);
+    
     
     AXoff = AXoff/numberOfTests;
     AYoff = AYoff/numberOfTests;
@@ -55,72 +65,28 @@ void calibrate(int numberOfTests){
 void init(void)
 {    
     Clock_Motor_PWM_Start();
-    PWM_LED_Start();
-    PWM_LED_BRIGHTNESS_Start();
-    
     /*USBUART_Start(0, USBUART_5V_OPERATION);
     USBUART_CDC_Init();
     while(USBUART_GetConfiguration() == 0) {}*/
     USBUART_Start();    
-    USBUART_PutString("COM Port Open\n\r");
+    USBUART_PutString("\n\rCOM Port Open\n\r");
     
     I2C_GY87_Start();    
     MPU6050_init();
 	MPU6050_initialize();
-    USBUART_PutString(MPU6050_testConnection() ? "MPU6050 connection successful\n\r" : "MPU6050 connection failed\n\n\r");
+    USBUART_PutString(MPU6050_testConnection() ? "MPU6050 connection successful\n\r" : "MPU6050 connection failed\n\n\r");   
     
     USBUART_PutString("Calbirating...\n\r");
     calibrate(100);
     USBUART_PutString("Calbiration done\n\r");
-}
-
-
-void getGyro(){
-    MPU6050_getMotion6t(&CAX, &CAY, &CAZ, &CGX, &CGY, &CGZ, &CT);
-    AX = (CAX-AXoff);
-    AY = (CAY-AYoff);
-    AZ = (CAZ-AZoff); 
     
-    GX = (CGX-GXoff);
-    GY = (CGY-GYoff);
-    GZ = (CGZ-GZoff);
-}
-
-/*void ftostrf (float val, char *sout) {
-    int index = 0;
+    Clock_Timer_G87_Sample_Start();
+    Clock_Timer_G87_Eval_Start();
+    Timer_GY87_Sample_Start();
+    Timer_GY87_Eval_Start();
     
-   
-    if((int)val > 0){
-        int decLen = log10(val);
-        for (int i = 0; i < decLen; i++)
-            sout[index++] = ((int)val / pow(10, decLen - i)) + '0';  
-        val /= pow(10, decLen);
-    } else
-        sout[index++] = '0'; 
-    
-    sout[index++] = '.';
-    
-    for (int i = 0; i < 4; i++){
-        val *= 10;
-        int digit = (int)val % 10;
-        char c = digit + '0';
-        sout[index++] =  c;
-    }        
-    sout[index] = 0;
-}*/
-
-void printGyro(){
-    /*char strAX[10] = "\0", strAY[10] = "\0", strAZ[10] = "\0", strGX[10] = "\0", strGY[10] = "\0", strGZ[10] = "\0";
-    ftostrf(AX, strAX);
-    ftostrf(AY, strAY);
-    ftostrf(AZ, strAZ);
-    ftostrf(GX, strGX);
-    ftostrf(GY, strGY);
-    ftostrf(GZ, strGZ);
-    
-    //sprintf(usbOutBuf, "AX:%.*f, AY:%.*f, AZ:%.*f || GX:%.*f, GY:%.*f, GZ:%.*f\n\r", 6,AX, 6,AY, 6,AZ, 6,GX, 6,GY, 6,GZ);
-    sprintf(usbOutBuf, "AX:%s, AY:%s, AZ:%s || GX:%s, GY:%s, GZ:%s\n\r", strAX,strAY,strAZ,strGX,strGY,strGZ);
-    USBUART_PutString(usbOutBuf);*/
+    PWM_LED_Start();
+    PWM_LED_BRIGHTNESS_Start();
 }
 
 
@@ -132,13 +98,37 @@ void setPWM(float mL, float mR){
 }
 
 
+CY_ISR(GyroSampleIT){
+    Timer_GY87_Sample_STATUS;
+    if(gyroState == GYRO_SAMPLE) {
+        MPU6050_getMotion6(&CAX, &CAY, &CAZ, &CGX, &CGY, &CGZ);
+        AX += (CAX-AXoff);
+        AY += (CAY-AYoff);
+        AZ += (CAZ-AZoff); 
+        
+        GX += (CGX-GXoff);
+        GY += (CGY-GYoff);
+        GZ += (CGZ-GZoff);
+        sampleCount++;
+    }
+}
+
+CY_ISR(GyroEvalIT){
+    Timer_GY87_Eval_STATUS;
+    if(gyroState == GYRO_SAMPLE)
+        gyroState = GYRO_EVAL; 
+}
+
 
 
 int main(void)
 {
-    CyGlobalIntEnable; /* Enable global interrupts. */
+    isr_Sample_GY87_StartEx(GyroSampleIT);
+    isr_Eval_GY87_StartEx(GyroEvalIT);    
     
-    init();       
+    init();   
+    
+    CyGlobalIntEnable; /* Enable global interrupts. */
     
     
     MotorState motorSate = MOTOR_SLEEPING;
@@ -147,12 +137,30 @@ int main(void)
     
     
     for(;;)
-    {
-        getGyro();
-        //printGyro();
-        
-        sprintf(usbOutBuf, "\rAX:%6d, AY:%6d, AZ:%6d || GX:%6d, GY:%6d, GZ:%6d     ", AX, AY, AZ, GX, GY, GZ);
-        USBUART_PutString(usbOutBuf);   
+    {        
+        switch(gyroState){
+            case GYRO_EVAL:
+                if(sampleCount > 0) {
+                    AX /= sampleCount;
+                    AY /= sampleCount;
+                    AZ /= sampleCount;
+                    GX /= sampleCount;
+                    GY /= sampleCount;
+                    GZ /= sampleCount;
+                    
+                    sprintf(usbOutBuf, "\rAX:%6d, AY:%6d, AZ:%6d || GX:%6d, GY:%6d, GZ:%6d     ", AX, AY, AZ, GX, GY, GZ);
+                    USBUART_PutString(usbOutBuf);  
+                    
+                    sampleCount = 0;
+                }                
+                AX = 0; AY = 0; AZ = 0;
+                GX = 0; GY = 0; GZ = 0;
+                
+                gyroState = GYRO_SAMPLE;
+                break;
+            default:
+                break;
+        }       
         
         switch(motorSate){
             case MOTOR_GO:
